eba89e6fa1c01b060972205177558e62ae68812a
Justin Tanacredi <Jtanacredi@gmail.com>
2014-03-21 19:30:48 -0400

    aded strong params for projects

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,16 @@
+class ProjectsController < ApplicationController
+  def index
+    @project = Project.all
+  end
+
+  def create
+    project = Project.create project_params
+  end
+
+  private
+
+  def user_params
+    params.require(:project).permit(:dash_image, :cover_image, :image_1, :image_2, :image_3, :image_4)
+  end
+
+end
