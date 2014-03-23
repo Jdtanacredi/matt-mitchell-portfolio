5b1425669807b2433e16026b26475d677523c27f
Justin Tanacredi <Jtanacredi@gmail.com>
2014-03-23 13:34:13 -0400

    Update Admin/Blogs_controller with show/create/new method. Create safe params

--- app/controllers/admin/blogs_controller.rb
+++ app/controllers/admin/blogs_controller.rb
@@ -2,4 +2,27 @@ class Admin::BlogsController < AdminsController
   def index
     @blogs = Blog.all
   end
+
+  def show
+    @blog = Blog.find params[:id]
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.create safe_blog
+  end
+
+  def edit
+    @blog = Blog.find params[:id]
+  end
+
+  private
+
+  def safe_blog
+    params.require(:blog).permit(:title, :headline_bold, :blog_description)
+  end
+
 end
