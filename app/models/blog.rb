d92aec7907dd0752df975343b045d6519cfeb4c4
Justin Tanacredi <Jtanacredi@gmail.com>
2014-03-30 12:42:46 -0400

    update blog model with dropbox credentials

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,6 @@
 class Blog < ActiveRecord::Base
+  has_attached_file :dash_image, :storage => :dropbox, :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+
+  validates_attachment_content_type :dash_image, :content_type => /\Aimage\/.*\Z/
+
 end
