ea921e266cb259a4d246737a1d634b1ee06e4a26
Justin Tanacredi <Jtanacredi@gmail.com>
2014-03-31 00:12:07 -0400

    update blog with validations

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,9 @@
 class Blog < ActiveRecord::Base
-  has_attached_file :blog_image, :storage => :dropbox, :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+  validates :title, presence: true, uniqueness: true
+  validates :headline_bold, presence: true, uniqueness: true
+  validates :blog_description, presence: true, uniqueness: true
 
+  has_attached_file :blog_image, :storage => :dropbox, :dropbox_credentials => Rails.root.join("config/dropbox.yml")
   validates_attachment_content_type :blog_image, :content_type => /\Aimage\/.*\Z/
 
 end
