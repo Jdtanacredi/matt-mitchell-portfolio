9874eca49a58054746d4973ec656af0d6a36c671
Justin Tanacredi <Jtanacredi@gmail.com>
2014-05-03 16:22:25 -0400

    update hover and mobile layout

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,4 +1,21 @@
-//$(document).ready(function () {
+$(document).ready(function () {
+
+    $('.behance').click(function(){
+        window.open('https://www.behance.net/meetchell');
+    });
+
+    $('.dribble').click(function(){
+        window.open('https://dribbble.com/mattMEETCHELL');
+    });
+
+    $('.linkedin').click(function(){
+        window.open('http://www.linkedin.com/pub/matthew-mitchell/38/a29/954');
+    });
+
+    $('.twitter').click(function(){
+        window.open('https://twitter.com/mattMEETCHELL');
+    });
+
 //
 //$('.style_image').hover(my_mouse_enter, my_mouse_leave);
 //
@@ -12,20 +29,19 @@
 //
 //});
 
-
-$(document).ready(function () {
-    var content = $('.hidden_content');
-    content.hide();
-
-    $('.style_image').mouseover(function () {
-//        $(this).show(content);
-        $(this).siblings(content).fadeIn('slow');
-        $(this).siblings().hide();
-        console.log('work');
-
-        $('.style_image').mouseout(function () {
-            content.hide();
-        });
-
-    });
+//$(document).ready(function () {
+//    var content = $('.hidden_content');
+//    content.hide();
+//
+//    $('.style_image').mouseover(function () {
+////        $(this).show(content);
+//        $(this).siblings(content).fadeIn('slow');
+//        $(this).siblings().hide();
+//        console.log('work');
+//
+//        $('.style_image').mouseout(function () {
+//            content.hide();
+//        });
+//
+//    });
 });
