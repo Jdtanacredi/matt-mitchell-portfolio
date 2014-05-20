421f04b76a8177b389ab14ef44662d50b620a50d
Justin Tanacredi <Jtanacredi@gmail.com>
2014-05-20 18:08:37 -0400

    add admin check with boolean var

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,7 +2,7 @@ class AdminsController < ApplicationController
   before_action :is_admin?
 
   def is_admin?
-    unless admin_signed_in?  #current_user.admin?
+    unless admin_signed_in? && current_admin.admin?
       redirect_to root_path
     end
   end
