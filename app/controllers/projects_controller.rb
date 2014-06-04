f3f02a829daa0e170d1c6e1763389c6d8ce640f2
Justin Tanacredi <Jtanacredi@gmail.com>
2014-06-04 00:12:20 -0400

    messing around with caching

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,38 @@
 class ProjectsController < ApplicationController
   def index
     @projects = Project.all
+    #in_cache = []
+    #@projects.each do |x|
+    #  x.cover_photo.url
+    #  in_cache << [{x.title => x.cover_photo.url}]
+    #end
   end
 
   def show
   @project = Project.find params[:id]
   end
 
+
+
+
+
+
+  #def lookup
+  #@projects = Project.all
+  #in_cache = []
+  #@projects.each do |x|
+  #  x.cover_photo.url
+  #  in_cache << [{x.title => x.cover_photo.url}]
+  #  if in_cache
+  #    @projects = in_cache
+  #    #message = "hit"
+  #  else
+  #    result = Geocoder.search(location_name).first
+  #    coordinates = "#{result.latitude},#{result.longitude}"
+  #    message = "miss"
+  #
+  #    Rails.cache.write(location_name, coordinates)
+  #  end
+  #end
 
 end
